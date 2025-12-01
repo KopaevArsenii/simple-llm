1f2bbeb8f2de5c074b4c69c9b54d9258b5bdf6f0
ArseniiKopaev <senya.kopaev@mail.ru>
2025-12-01 16:23:15 +0300

    removed useless routes

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -7,10 +7,8 @@ export const Route = createRootRoute({
 function RootComponent() {
   return (
     <div className="flex min-h-screen">
-      <nav className="w-[200px] bg-gray-800 text-white p-4 flex flex-col gap-4">
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/contact">Contact</a>
+      <nav className="w-[200px] bg-black text-white p-4 flex flex-col gap-4">
+        <a href="/">New chat</a>
       </nav>
       <main className="flex-1 bg-gray-100">
         <Outlet />
